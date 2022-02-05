c8b2d9210c1c38ecbd7312ccb82b0c78f93112f2
Udit Karode <udit.karode@gmail.com>
2022-02-05 15:42:07 +0530

    feat(routes/board): update/delete

--- backend/src/database/entities/board/utils.rs
+++ backend/src/database/entities/board/utils.rs
@@ -17,7 +17,34 @@ pub fn create_board<'a>(conn: &SqliteConnection, name: &'a String) -> Result<usi
     Ok(result)
 }
 
-pub fn get_boards<'a>(conn: &SqliteConnection) -> Result<Vec<Board>, DbError> {
+pub fn get_from_id(conn: &SqliteConnection, cid: String) -> Result<Board, DbError> {
+    use crate::database::schema::boards::dsl::*;
+    let res = boards.filter(id.eq(cid)).first::<Board>(conn)?;
+
+    Ok(res)
+}
+
+pub fn delete(conn: &SqliteConnection, cid: String) -> Result<usize, DbError> {
+    use crate::database::schema::boards::dsl::*;
+    let res = diesel::delete(boards.filter(id.eq(cid))).execute(conn)?;
+
+    Ok(res)
+}
+
+pub fn update_name(
+    conn: &SqliteConnection,
+    cid: String,
+    new_name: String,
+) -> Result<usize, DbError> {
+    use crate::database::schema::boards::dsl::*;
+    let res = diesel::update(boards.filter(id.eq(cid)))
+        .set(name.eq(new_name))
+        .execute(conn)?;
+
+    Ok(res)
+}
+
+pub fn get_boards(conn: &SqliteConnection) -> Result<Vec<Board>, DbError> {
     use crate::database::schema::boards::dsl::*;
     let res = boards.load::<Board>(conn)?;
 
--- backend/src/routes/board.rs
+++ backend/src/routes/board.rs
@@ -1,15 +1,13 @@
 use crate::utils::misc::from_blocking_err;
 use crate::utils::models::AppError;
 use crate::{database::entities::board, utils::models::State};
-use actix_web::{get, post, web, HttpRequest, HttpResponse};
+use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse};
 use serde_json::json;
 
+type Resp = Result<HttpResponse, AppError>;
+
 #[post("/board")]
-async fn create(
-    state: web::Data<State>,
-    req: HttpRequest,
-    name: String,
-) -> Result<HttpResponse, AppError> {
+async fn create(state: web::Data<State>, req: HttpRequest, name: String) -> Resp {
     let log = &state.log.clone();
 
     web::block(move || {
@@ -24,8 +22,40 @@ async fn create(
     })))
 }
 
+#[put("/board")]
+async fn update(state: web::Data<State>, req: HttpRequest, cid: String, new_name: String) -> Resp {
+    let log = &state.log.clone();
+
+    web::block(move || {
+        let conn = state.pool.get()?;
+        board::utils::update_name(&conn, cid, new_name)
+    })
+    .await
+    .map_err(|e| from_blocking_err(e, log, req))?;
+
+    Ok(web::HttpResponse::Ok().json(json!({
+        "success": true,
+    })))
+}
+
+#[delete("/board")]
+async fn delete(state: web::Data<State>, req: HttpRequest, cid: String) -> Resp {
+    let log = &state.log.clone();
+
+    web::block(move || {
+        let conn = state.pool.get()?;
+        board::utils::delete(&conn, cid)
+    })
+    .await
+    .map_err(|e| from_blocking_err(e, log, req))?;
+
+    Ok(web::HttpResponse::Ok().json(json!({
+        "success": true,
+    })))
+}
+
 #[get("/board")]
-async fn read(state: web::Data<State>, req: HttpRequest) -> Result<HttpResponse, AppError> {
+async fn read(state: web::Data<State>, req: HttpRequest) -> Resp {
     let log = &state.log.clone();
 
     let boards = web::block(move || {
